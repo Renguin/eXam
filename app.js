888a64b71304de69b7d88dfffe0a987170d816b8
Renguin <maityrahul7@gmail.com>
2020-07-17 17:14:08 +0530

    Added app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+          app = express(),
+   bodyParser = require("body-parser"),
+     mongoose = require("mongoose");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+const PORT = process.env.PORT || 8000;
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`app is listening on PORT ${PORT}`);
+});
